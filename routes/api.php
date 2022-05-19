28bd53fe18a8d717163e4d97bb64c600da878d0d
WhiteMalina <49318023+whitemalina@users.noreply.github.com>
2022-05-19 21:51:32 +0500

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\CardController;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +17,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('login', [AuthController::class, 'login'])->name('login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('card/{url}', [CardController::class,'url']);
+
+Route::get('show/cards', [CardController::class,'cards']);
+
+Route::middleware('auth:sanctum')->group( function () {
+
+    Route::get('user', [AuthController::class, 'index']);
+
+    Route::post('update', [AuthController::class, 'update']);
+
+    Route::resource('cards', CardController::class);
+
+    Route::resource('tasks', TaskController::class);
 });
+
+
+Route::post('register', [AuthController::class, 'register']);
